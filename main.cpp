16ad1f7570ed8f4cedb8ac554ea74f10dab789b8
Lorismelik <t9660121@yandex.ru>
2015-04-08 00:40:02 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,19 +1,14 @@
 #include "templateset.h"
 void main(void)
-	{
-	   MySet <char> st(6);
-	   char i = '3';
-	   st.add(i);
-	   cout << st << endl;
-	   bool b = st.contains(i);
-	   cout << boolalpha << b << endl;
-	   st.remove(i);
-	   int c=st.size();
-	   cout << c << endl;
-	   cout << st << endl;
-	   i = '7';
-	   st.add(i);
-	   cout << st << endl;
-	   c = st.size();
-	   cout << c << endl;
-	} 
+{
+	MySet <char> st;
+	char i = '3';
+	st.add(i);
+	cout << st << endl;
+	bool b = st.contains(i);
+	cout << boolalpha << b << endl;
+	st.remove(i);
+	int c = st.size();
+	cout << c << endl;
+	cout << st << endl;
+}
