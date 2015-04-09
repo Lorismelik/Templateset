e2745b81f847ecb878c61364a3e57372d83c1d19
Lorismelik <t9660121@yandex.ru>
2015-04-10 01:25:49 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,23 +1,6 @@
-#include "templateset.h"
+#include "templatebintree.h"
 void main(void)
 {
 	MySet <char> st;
 	char i = '3';
-	st.add(i);
-	cout << st << endl;
-	bool b = st.contains(i);
-	cout << boolalpha << b << endl;
-	int c = st.size();
-	cout << c << endl;
-	st.remove(i);
-	c = st.size();
-	cout << c << endl;
-	cout << st << endl;
-	i = '6';
-	st.add(i);
-	i = '7';
-	st.add(i);
-	MySet <char> sr;
-	sr = st;
-	cout << sr;
 }
