5b7c328f33c2aec426e3bd7de4b931eda5f9eb09
Lorismelik <t9660121@yandex.ru>
2015-04-10 01:54:10 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,23 @@
 #include "templatebintree.h"
-void main(void)
+void main()
 {
-	MySet <char> st;
-	char i = '3';
+	MySet <int> st;
+	int i = 3;
+	st.add(i);
+	i = 2;
+	st.add(i);
+	i = 33;
+	st.add(i);
+	i = 0;
+	st.add(i);
+	i = 6;
+	st.add(i);
+	st.show(std::cout);
+	std::cout << "\n";
+	st.remove(i);
+	st.show(std::cout);
+	i = 3;
+	std::cout << "\n";
+	st.remove(i);
+	st.show(std::cout);
 }
